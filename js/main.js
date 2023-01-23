76bfdd8c1b7b30c132ed9848afb312db73472e79
jhechus <jess.osm1412@gmail.com>
2023-01-23 16:19:44 -0600

    primer comit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/* detectar el ancho de la imagen en diamante y poner el mismo valor en height */
+
+var dn_width = $('.diamante').width();
+$(".diamante").css({
+    height: dn_width -40
+})
+console.log(dn_width);
+
+
+/* agregar scroll a la barra de navegacion */
+var nav = document.querySelector('nav');
+window.addEventListener('scroll',function (){
+    if(window.pageYOffset > 500){
+        nav.classList.add("nav_overlay");
+    }else{
+        nav.classList.remove("nav_overlay");
+    }
+});
+
+
+    
