a650df6c3afff67f05ac4933dd5fa170379d50e4
jhechus <jess.osm1412@gmail.com>
2023-01-23 18:40:21 -0600

    acabando estilos del portafolio, un paso antes de agregar el scroll de js a cada pagina diferente

--- js/main.js
+++ js/main.js
@@ -4,6 +4,11 @@ var dn_width = $('.diamante').width();
 $(".diamante").css({
     height: dn_width -40
 })
+
+var dn_width = $('.grid-item').width();
+$(".grid-item").css({
+    height: dn_width -40
+})
 console.log(dn_width);
 
 
